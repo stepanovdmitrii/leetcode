7cf00c06e160bb0d136c94142f0206be6ce2f7ad
Dmitrii <dima-stepan@yandex.ru>
2020-08-09 22:52:16 +0300

    sorting & searching completed

---
+++ LeetCode/SortingAndSearching/FirstBadVersionSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.SortingAndSearching
+{
+    abstract class VersionControl
+    {
+        protected bool IsBadVersion(int version) => DateTime.UtcNow.Ticks % 2 == 0;
+    }
+    class FirstBadVersionSolution: VersionControl
+    {
+        public int FirstBadVersion(int n)
+        {
+            int l = 0;
+            int r = n - 1;
+            while(l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                if(IsBadVersion(mid + 1))
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            return l + 1;
+        }
+    }
+}
---
+++ LeetCode/SortingAndSearching/MergeSortedArraySolution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.SortingAndSearching
+{
+    class MergeSortedArraySolution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            if (n == 0) return;
+            if (m == 0)
+            {
+                for (int i = 0; i < n; ++i)
+                {
+                    nums1[i] = nums2[i];
+                }
+                return;
+            }
+
+            int target = m + n - 1;
+            int index1 = m - 1;
+            int index2 = n - 1;
+
+            while (index1 >= 0 || index2 >= 0)
+            {
+                if(index1 < 0)
+                {
+                    nums1[target] = nums2[index2];
+                    --index2;
+                }
+                else if (index2 < 0)
+                {
+                    nums1[target] = nums1[index1];
+                    --index1;
+                }
+                else if(nums2[index2] > nums1[index1])
+                {
+                    nums1[target] = nums2[index2];
+                    --index2;
+                }
+                else
+                {
+                    nums1[target] = nums1[index1];
+                    --index1;
+                }
+                --target;
+            }
+        }
+    }
+}
