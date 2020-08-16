59233a215aecb7f9d895d732cbb3f74d63a56f7f
Dmitrii <dima-stepan@yandex.ru>
2020-08-16 17:56:47 +0300

    design completed

---
+++ LeetCode/Design/MinStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Design
+{
+    public class MinStack
+    {
+        private readonly Stack<int> _values = new Stack<int>();
+        private readonly Stack<int> _min = new Stack<int>();
+
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+            _values.Push(x);
+            if(_min.Count == 0)
+            {
+                _min.Push(x);
+            }
+            else
+            {
+                _min.Push(Math.Min(_min.Peek(), x));
+            }
+        }
+
+        public void Pop()
+        {
+            _values.Pop();
+            _min.Pop();
+        }
+
+        public int Top()
+        {
+            return _values.Peek();
+        }
+
+        public int GetMin()
+        {
+            return _min.Peek();
+        }
+    }
+}
---
+++ LeetCode/Design/ShuffleAnArraySolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Design
+{
+    class ShuffleAnArraySolution
+    {
+        private int[] _nums;
+        private int[] _shuffled;
+        private readonly Random _random;
+
+        public ShuffleAnArraySolution(int[] nums)
+        {
+            _nums = nums;
+            _shuffled = new int[_nums.Length];
+            Array.Copy(_nums, _shuffled, _nums.Length);
+            _random = new Random();
+        }
+
+        /** Resets the array to its original configuration and return it. */
+        public int[] Reset()
+        {
+            return _nums;
+        }
+
+        /** Returns a random shuffling of the array. */
+        public int[] Shuffle()
+        {
+            for(int i = _shuffled.Length - 1; i > 0; --i)
+            {
+                int j = _random.Next(0, i + 1);
+                int tmp = _shuffled[i];
+                _shuffled[i] = _shuffled[j];
+                _shuffled[j] = tmp;
+            }
+            return _shuffled;
+        }
+    }
+}
