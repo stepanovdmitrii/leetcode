0cf1fcab7d03acd3787e4b8921f4e2193c660a53
Dmitrii <dima-stepan@yandex.ru>
2020-11-04 19:08:45 +0300

    binary search template II completed

---
+++ LeetCode/BinarySearch/FindPeakElementSolution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.BinarySearch
+{
+    class FindPeakElementSolution
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while(left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if(nums[mid] > nums[mid + 1])
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+    }
+}
---
+++ LeetCode/BinarySearch/RotatedSortedArrayMinimum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.BinarySearch
+{
+    class RotatedSortedArrayMinimum
+    {
+        public int FindMin(int[] nums)
+        {
+            if (nums.Length == 1) return nums[0];
+            int pivot = FindPivot(nums);
+            return nums[0] < nums[pivot] ? nums[0] : nums[pivot];
+        }
+
+        private int FindPivot(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > nums[left])
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+            return left + 1;
+        }
+    }
+}
