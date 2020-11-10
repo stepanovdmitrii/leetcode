e9076e60b557865db858877b3831eb91a20096cd
Dmitrii <dima-stepan@yandex.ru>
2020-11-10 22:09:07 +0300

    templates completed

---
+++ LeetCode/BinarySearch/FindClosestElementsSolution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.BinarySearch
+{
+    class FindClosestElementsSolution
+    {
+        public IList<int> FindClosestElements(int[] arr, int k, int x)
+        {
+            if (k <= 0) return new List<int>();
+            k = k > arr.Length ? arr.Length : k;
+            int lowerBound = LowerBound(arr, x);
+            if (lowerBound == -1)
+            {
+                return arr.TakeLast(k).ToList();
+            }
+            else if (lowerBound == 0)
+            {
+                return arr.Take(k).ToList();
+            }
+            else
+            {
+                var result = new LinkedList<int>();
+                int left = lowerBound - 1;
+                int right = lowerBound;
+                while(k > 0)
+                {
+                    if(left < 0)
+                    {
+                        result.AddLast(arr[right]);
+                        ++right;
+                    }
+                    else if (right == arr.Length)
+                    {
+                        result.AddFirst(arr[left]);
+                        --left;
+                    }
+                    else
+                    {
+                        int leftDiff = x - arr[left];
+                        int rightDiff = arr[right] - x;
+                        if(rightDiff < leftDiff)
+                        {
+                            result.AddLast(arr[right]);
+                            ++right;
+                        }
+                        else
+                        {
+                            result.AddFirst(arr[left]);
+                            --left;
+                        }
+                    }
+                    --k;
+                }
+                return result.ToList();
+            }
+        }
+        int LowerBound(int[] arr, int target)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            while(left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if(arr[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return left < arr.Length ? left : -1;
+        }
+    }
+}
---
+++ LeetCode/BinarySearch/RangeSearchSolution.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.BinarySearch
+{
+    public sealed class RangeSearchSolution
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            int lowerBound = LowerBound(nums, target);
+            if(lowerBound == -1 || nums[lowerBound] > target) return new[] { -1, -1};
+
+            int upperBound = UpperBound(nums, target);
+            if (upperBound == -1) return new[] { lowerBound, nums.Length - 1 };
+            return new[] { lowerBound, upperBound - 1 };
+        }
+
+        private int LowerBound(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return left < nums.Length ? left : -1;
+        }
+
+        private int UpperBound(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while(left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if(target >= nums[mid])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return left < nums.Length ? left : -1;
+        }
+    }
+}
