afa4338c1c3115b2b4e4c403a019e17c8b82c325
Dmitrii Stepanov <dima-stepan@yandex.ru>
2020-12-02 21:32:21 +0300

    number of islands

---
+++ LeetCode/LinkedList/RandomNodeSolution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.LinkedList
+{
+    class RandomNodeSolution
+    {
+        public class Solution
+        {
+            private readonly Random _random;
+            private readonly int _length;
+            private readonly ListNode _head;
+
+            /** @param head The linked list's head.
+                Note that the head is guaranteed to be not null, so it contains at least one node. */
+            public Solution(ListNode head)
+            {
+                _random = new Random();
+                _length = GetLength(head);
+                _head = head;
+            }
+
+            private static int GetLength(ListNode head)
+            {
+                int count = 0;
+                ListNode current = head;
+                while(current != null)
+                {
+                    ++count;
+                    current = current.next;
+                }
+                return count;
+            }
+
+            /** Returns a random node's value. */
+            public int GetRandom()
+            {
+                int position = _random.Next(0, _length);
+                int current = 0;
+                ListNode node = _head;
+                while(current != position)
+                {
+                    node = node.next;
+                    ++current;
+                }
+                return node.val;
+            }
+        }
+    }
+}
--- LeetCode/Queue/NumIslandsSolution.cs
+++ LeetCode/Queue/NumIslandsSolution.cs
@@ -7,28 +7,35 @@ namespace LeetCode.Queue
 {
     class NumIslandsSolution
     {
-        struct Index
-        {
-            public int Row;
-            public int Column;
-        }
-
-        class Comparer : IEqualityComparer<Index>
-        {
-            public bool Equals(Index x, Index y)
-            {
-                return x.Row == y.Row && x.Column == y.Column;
-            }
-
-            public int GetHashCode(Index obj)
-            {
-                return HashCode.Combine<int, int>(obj.Row, obj.Column);
-            }
-        }
-
         public int NumIslands(char[][] grid)
         {
+            if (grid == null || grid.Length == 0 || grid[0].Length == 0) return 0;
 
+            int result = 0;
+            for(int row = 0; row < grid.Length; ++row)
+            {
+                for(int col = 0; col < grid[row].Length; ++col)
+                {
+                    if (grid[row][col] == '1')
+                    {
+                        Visit(grid, row, col, grid.Length, grid[0].Length);
+                        ++result;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static void Visit(char[][] grid, int row, int col, int rows, int cols)
+        {
+            if(row >= 0 && row < rows && col >= 0 && col < cols && grid[row][col] == '1')
+            {
+                grid[row][col] = '0';
+                Visit(grid, row + 1, col, rows, cols);
+                Visit(grid, row - 1, col, rows, cols);
+                Visit(grid, row, col + 1, rows, cols);
+                Visit(grid, row, col - 1, rows, cols);
+            }
         }
     }
 }
