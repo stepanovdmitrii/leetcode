b19d8e99c1c021f916f98d4a9c624be97d82e3ae
Dmitrii <dima-stepan@yandex.ru>
2020-12-20 18:29:30 +0300

    decode at index

---
+++ LeetCode.UnitTests/DecodeAtIndexTests.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Strings;
+using NUnit.Framework;
+
+namespace LeetCode.UnitTests
+{
+    [TestFixture]
+    public sealed class DecodeAtIndexTests
+    {
+        [Test]
+        public void Test_Case3()
+        {
+            var solution = new DecodeAtIndexSolution();
+            Assert.AreEqual("o", solution.DecodeAtIndex("leet2code3", 10));
+        }
+
+        [Test]
+        public void Test_Case33()
+        {
+            var solution = new DecodeAtIndexSolution();
+            Assert.AreEqual("y", solution.DecodeAtIndex("y959q969u3hb22odq595", 222280369));
+        }
+
+        [Test]
+        public void Test_Case2()
+        {
+            var solution = new DecodeAtIndexSolution();
+            Assert.AreEqual("a", solution.DecodeAtIndex("a2345678999999999999999", 1));
+        }
+
+        [Test]
+        public void Test_Case11()
+        {
+            var solution = new DecodeAtIndexSolution();
+            Assert.AreEqual("a", solution.DecodeAtIndex("a23", 6));
+        }
+    }
+}
---
+++ LeetCode/Strings/DecodeAtIndexSolution.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Strings
+{
+    public sealed class DecodeAtIndexSolution
+    {
+        private sealed class Iterator : IEnumerable<char>
+        {
+            private readonly string _input;
+            private readonly int _tailStart;
+            private readonly int _tailCount;
+            private readonly IEnumerable<char> _previous;
+            private readonly int _repeat;
+
+            public Iterator(string input, int tailStart, int tailCount, int repeat, IEnumerable<char> previous)
+            {
+                _input = input;
+                _tailStart = tailStart;
+                _tailCount = tailCount;
+                _repeat = repeat;
+                _previous = previous;
+            }
+
+            public IEnumerator<char> GetEnumerator()
+            {
+                for(int repeat = 0; repeat < _repeat; ++repeat)
+                {
+                    foreach(var ch in _previous)
+                    {
+                        yield return ch;
+                    }
+                    for(int idx = _tailStart; idx < _tailStart + _tailCount; ++idx)
+                    {
+                        yield return _input[idx];
+                    }
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+        public string DecodeAtIndex(string S, int K)
+        {
+            long size = 0;
+            foreach(var ch in S)
+            {
+                if (char.IsDigit(ch))
+                {
+                    size *= long.Parse(ch.ToString());
+                }
+                else
+                {
+                    size++;
+                }
+            }
+
+            long k = K - 1;
+
+            for(int idx = S.Length - 1; idx >= 0; --idx)
+            {
+                if (char.IsDigit(S[idx]))
+                {
+                    long mult = long.Parse(S[idx].ToString());
+                    long baseSize = size / mult;
+                    if(k >= baseSize && k < size)
+                    {
+                        k = k % baseSize;
+                    }
+                    size = baseSize;
+                }
+                else
+                {
+                    --size;
+                    if(size == k)
+                    {
+                        return S[idx].ToString();
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private static IEnumerable<char> Enumerate(string S)
+        {
+            var result = Enumerable.Empty<char>();
+
+            int tailStart = 0;
+            int tailCount = 0;
+            for(int idx = 0; idx < S.Length; ++idx)
+            {
+                if (char.IsDigit(S[idx]))
+                {
+                    int repeat = int.Parse(S[idx].ToString());
+                    var next = new Iterator(S, tailStart, tailCount, repeat, result);
+                    result = next;
+                    tailCount = tailStart = 0;
+                }
+                else
+                {
+                    if(tailCount == 0)
+                    {
+                        tailStart = idx;
+                    }
+                    ++tailCount;
+                }
+            }
+
+            if(tailCount > 0)
+            {
+                var next = new Iterator(S, tailStart, tailCount, 1, result);
+                result = next;
+            }
+
+            return result;
+        }
+    }
+}
