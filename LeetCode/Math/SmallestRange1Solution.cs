4d7cbdd37ddb9b057f4beb6ff487aa1336d3b150
Dmitrii <dima-stepan@yandex.ru>
2020-12-21 20:22:46 +0300

    smallest range solutions

---
+++ LeetCode/Math/SmallestRange1Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Math
+{
+    class SmallestRange1Solution
+    {
+        public int SmallestRangeI(int[] A, int K)
+        {
+            Array.Sort(A);
+            int min = A[0];
+            int max = A[A.Length - 1];
+            int result = max - min - 2 * K;
+            if (result > 0) return result;
+            return 0;
+        }
+    }
+}
---
+++ LeetCode/Math/SmallestRange2Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Math
+{
+    class SmallestRange2Solution
+    {
+        public int SmallestRangeII(int[] A, int K)
+        {
+            Array.Sort(A);
+            int ans = A[A.Length - 1] - A[0];
+
+            for (int i = 0; i < A.Length - 1; ++i)
+            {
+                int current = A[i];
+                int right = A[i + 1];
+                int max = System.Math.Max(A[A.Length - 1] - K, current + K);
+                int min = System.Math.Min(A[0] + K, right - K);
+                ans = System.Math.Min(ans, max - min);
+            }
+            return ans;
+        }
+    }
+}
