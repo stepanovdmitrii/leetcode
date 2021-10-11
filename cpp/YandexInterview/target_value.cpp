eb21560f1c4ba2df81d0eb955e53938cbf97635e
Dmitrii <dima-stepan@yandex.ru>
2021-10-11 21:44:49 +0300

    tasks

---
+++ cpp/YandexInterview/target_value.cpp
@@ -0,0 +1,9 @@
+
+
+
+/*
+Дан список интов и число-цель. Нужно найти такой range, чтобы сумма его элементов давала число-цель.
+elements = [1, -3, 4, 5]
+target = 9
+result = range(2, 4) # because elements[2] + elements[3] == target
+*/
